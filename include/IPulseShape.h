5c364c8df5f2eb764c0b32ef848486691537387e
Guilherme Goncalves <inacio.guilherme@gmail.com>
2022-04-16 11:39:57 -0300

    Fix IPulseShape

--- include/IPulseShape.h
+++ include/IPulseShape.h
@@ -5,17 +5,19 @@
 
 class IPulseShape
 {
-  protected:
-    std::vector<double> m_ySeries;
-    std::vector<double> m_tSeries;
-  private:
-    unsigned int TimeToIndex(const double & time) const;
   public:
+    IPulseShape() = default;
+    virtual ~IPulseShape() = default;
     double GetY(const double & time) const;
     std::vector<double> GetTimeSeries() const { return m_tSeries; }
     std::vector<double> GetYSeries() const { return m_ySeries; }
     virtual float GetResolution() const = 0;
     virtual unsigned int GetSize() const = 0;
+  protected:
+    std::vector<double> m_ySeries;
+    std::vector<double> m_tSeries;
+  private:
+    unsigned int TimeToIndex(const double & time) const;
 };
 
 #endif /* I_PULSE_SHAPE_H */
