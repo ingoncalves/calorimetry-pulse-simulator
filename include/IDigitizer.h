41386f92c6134165b52fee46ce5a859827f7ba66
Guilherme Goncalves <inacio.guilherme@gmail.com>
2022-09-20 20:13:36 -0300

    Add GNU GPL License

--- include/IDigitizer.h
+++ include/IDigitizer.h
@@ -1,3 +1,22 @@
+/**
+ * @file include/IDigitizer.h
+ * @author Guilherme Inacio Goncalves <ggoncalves@iprj.uer.br>
+ * @copyright Copyright 2022, Guilherme Inacio Goncalves. All rights reserved.
+ *
+ * This file is part of CPS (Calorimetry Pulse Simulator).
+ *
+ * CPS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * CPS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * CPS. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef I_DIGITIZER_H
 #define I_DIGITIZER_H
 
