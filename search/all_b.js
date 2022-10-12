42ef096e70798c6dd3214669577222323bc80599
ingoncalves <ingoncalves@users.noreply.github.com>
2022-10-12 00:12:34 +0000

    deploy: 641c0da37782ad988d0b0993beeb8fa0395c6a08

--- search/all_b.js
+++ search/all_b.js
@@ -1,16 +1,16 @@
 var searchData=
 [
   ['samples_44',['samples',['../structcps_1_1ContinuousDataset.html#a00ed8173c4be4b04687d8214bb3a599e',1,'cps::ContinuousDataset']]],
-  ['setamplitudedistribution_45',['SetAmplitudeDistribution',['../classcps_1_1PulseGenerator.html#ad7145dea175191791d357169cab761bf',1,'cps::PulseGenerator']]],
-  ['setdeformationlevel_46',['SetDeformationLevel',['../classcps_1_1PulseGenerator.html#a5f0a6fa325be032d9fd4b77dc1caf9a6',1,'cps::PulseGenerator']]],
-  ['seteventsscheme_47',['SetEventsScheme',['../classcps_1_1DatasetGenerator.html#a5d2ea4cba8ad5fbdc4aab1acf6af0fc8',1,'cps::DatasetGenerator']]],
-  ['setnoiseparams_48',['SetNoiseParams',['../classcps_1_1DatasetGenerator.html#ab2f9156a89d356cb9aeba448bcfaa493',1,'cps::DatasetGenerator::SetNoiseParams()'],['../classcps_1_1PulseGenerator.html#a842f603cf8a97746335aa48e60284253',1,'cps::PulseGenerator::SetNoiseParams()']]],
-  ['setnsamples_49',['SetNSamples',['../classcps_1_1Digitizer.html#a0243033aa88a774904cf5a73226db8c7',1,'cps::Digitizer']]],
-  ['setoccupancy_50',['SetOccupancy',['../classcps_1_1DatasetGenerator.html#a2d88408fde79062f932582a93e064e24',1,'cps::DatasetGenerator']]],
-  ['setpedestal_51',['SetPedestal',['../classcps_1_1PulseGenerator.html#a7444241ef012e5eb77a0d0f6ac1998b5',1,'cps::PulseGenerator']]],
-  ['setphasedistribution_52',['SetPhaseDistribution',['../classcps_1_1PulseGenerator.html#afdf606ae2e421d3ad26e60ae84227aba',1,'cps::PulseGenerator']]],
-  ['setpulsegenerator_53',['SetPulseGenerator',['../classcps_1_1DatasetGenerator.html#a0533a31b616dd93891f5d833c390c897',1,'cps::DatasetGenerator']]],
-  ['setsamplingrate_54',['SetSamplingRate',['../classcps_1_1DatasetGenerator.html#a7023158eba7910ad6ad7d90cec583863',1,'cps::DatasetGenerator::SetSamplingRate()'],['../classcps_1_1Digitizer.html#a8c822aa795adf76d4112dc1f4abf0a91',1,'cps::Digitizer::SetSamplingRate()']]],
-  ['setstartsamplingattime_55',['SetStartSamplingAtTime',['../classcps_1_1Digitizer.html#a2a203cc6ebb336ecf067beb7876886f1',1,'cps::Digitizer']]],
+  ['setamplitudedistribution_45',['SetAmplitudeDistribution',['../classcps_1_1PulseGenerator.html#a901ceda2aa37778048fb6a2fe7947a2b',1,'cps::PulseGenerator']]],
+  ['setdeformationlevel_46',['SetDeformationLevel',['../classcps_1_1PulseGenerator.html#a55fcaa785d64c4cf73163e2b99b03457',1,'cps::PulseGenerator']]],
+  ['seteventsscheme_47',['SetEventsScheme',['../classcps_1_1DatasetGenerator.html#adc0815b29b79ce5ed3fd2aed7bfa3688',1,'cps::DatasetGenerator']]],
+  ['setnoiseparams_48',['SetNoiseParams',['../classcps_1_1DatasetGenerator.html#a038352b3d03a1ba0ebaccea8c206026c',1,'cps::DatasetGenerator::SetNoiseParams()'],['../classcps_1_1PulseGenerator.html#a1c2eea3054a89b3e0ae4d3d3bc40ce34',1,'cps::PulseGenerator::SetNoiseParams()']]],
+  ['setnsamples_49',['SetNSamples',['../classcps_1_1Digitizer.html#ab82b31a27febc25cb03254cf2f6f5677',1,'cps::Digitizer']]],
+  ['setoccupancy_50',['SetOccupancy',['../classcps_1_1DatasetGenerator.html#aab661abdb80cc6baee5e77cb64822e1d',1,'cps::DatasetGenerator']]],
+  ['setpedestal_51',['SetPedestal',['../classcps_1_1PulseGenerator.html#a6241362c698fefca32f4b6e5c1511740',1,'cps::PulseGenerator']]],
+  ['setphasedistribution_52',['SetPhaseDistribution',['../classcps_1_1PulseGenerator.html#a45d6f5049ecf483a9d6c5bd6f88c8123',1,'cps::PulseGenerator']]],
+  ['setpulsegenerator_53',['SetPulseGenerator',['../classcps_1_1DatasetGenerator.html#a60ffb8c68bb5b18fcc4e079182e61b4d',1,'cps::DatasetGenerator']]],
+  ['setsamplingrate_54',['SetSamplingRate',['../classcps_1_1DatasetGenerator.html#a253f2a7f4abdbf50502d352a97b09696',1,'cps::DatasetGenerator::SetSamplingRate()'],['../classcps_1_1Digitizer.html#abc9c7d4b04f8643a15f4b4c53aac4c9a',1,'cps::Digitizer::SetSamplingRate()']]],
+  ['setstartsamplingattime_55',['SetStartSamplingAtTime',['../classcps_1_1Digitizer.html#a08b9cfff3eebe2d4cb62d4cd132748ad',1,'cps::Digitizer']]],
   ['sliceddataset_56',['SlicedDataset',['../structcps_1_1SlicedDataset.html',1,'cps']]]
 ];
